fadc136424c9e72f7ccb7a7726d967721d9a9dfa
Vadim <kalugavadim04@gmail.com>
2024-09-08 20:24:51 +0700

    refactor: caching of receiving task types (priorities)

--- src/main/kotlin/com/vadim/mytask/service/PriorityServiceImpl.kt
+++ src/main/kotlin/com/vadim/mytask/service/PriorityServiceImpl.kt
@@ -3,11 +3,13 @@ package com.vadim.mytask.service
 import com.vadim.mytask.dto.Priority
 import com.vadim.mytask.entity.PriorityEntity
 import com.vadim.mytask.repository.PriorityRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class PriorityServiceImpl(
     private val priorityRepository: PriorityRepository
 ) : PriorityService {
+    @Cacheable("taskPrioritiesCache")
     override fun getAll(): List<Priority> = priorityRepository.findByOrderByPlevelDesc().map { it.toDto() }
 }
